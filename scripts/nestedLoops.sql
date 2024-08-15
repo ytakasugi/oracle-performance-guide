c774412f18c58b4cc656b8f68980a462783f62fd
ytakasugi <sh7.tibi0129@gmail.com>
2024-08-16 00:11:36 +0900

    change file

--- scripts/nestedLoops.sql
+++ scripts/nestedLoops.sql
@@ -9,7 +9,6 @@ FROM
     ON E.DEPT_ID = D.DEPT_ID
 ;
 
-
 -- 内部表のインデックスが使用されないNested Loops
 SELECT /*+ LEADING(E D) USE_NL(D) FULL(D) */ 
   E.EMP_ID
@@ -21,13 +20,3 @@ FROM
     ON E.DEPT_ID = D.DEPT_ID
 ;
 
--- 内部表のインデックスが使用されないNested Loops
-SELECT /*+ LEADING(D E) USE_HASH(E) */ 
-  E.EMP_ID
-  , E.EMP_NAME
-  , E.DEPT_ID
-  , D.DEPT_NAME
-FROM 
-  EMPLOYEES E INNER JOIN DEPARTMENTS D
-    ON E.DEPT_ID = D.DEPT_ID
-;
